4db6d52dd27bb22a08d36343504cd2127534a4d4
krish koli <krishkoli536@gmail.com>
2025-07-01 01:51:10 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,62 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import Navbar from '@/components/layout/Navbar'
+import NextTopLoader from 'nextjs-toploader';
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "NoteSync",
+  description: "Capture your ideas, anytime, anywhere.",
+  keywords: ["NoteSync", "note-taking app", "online notes", "cloud notes", "secure notes", "productivity"],
+  authors: [{ name: "Vinod Kumar", url: "https://www.vinodjangid.site" }],
+  creator: "Vinod Kumar",
+  openGraph: {
+    title: "NoteSync - Capture Your Ideas, Anytime, Anywhere",
+    description: "A cloud-based note-taking app that helps you organize and access your notes securely from anywhere.",
+    url: "https://notesync-site.netlify.app/",
+    siteName: "NoteSync",
+    images: [
+      {
+        url: "https://notesync-site.netlify.app/images/ogimage.png",
+        width: 1200,
+        height: 630,
+        alt: "NoteSync - Capture Your Ideas, Anytime, Anywhere",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "NoteSync - Cloud-Based Note-Taking App",
+    description: "Capture your ideas effortlessly with NoteSync. Secure, fast, and accessible anytime, anywhere.",
+    images: ["https://notesync-site.netlify.app/images/ogimage.png"],
+    creator: "@Vinod_Jangid07",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <NextTopLoader  color="#9333ea" showSpinner={false}/>
+        <Navbar/>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ next.config.ts
@@ -0,0 +1,24 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'i.pravatar.cc',
+//       },
+//     ],
+//   },
+// };
+
+// module.exports = nextConfig;
+
+
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['egwcldgnufylvhvelkqu.supabase.co'],
+  },
+};
+
+module.exports = nextConfig;
